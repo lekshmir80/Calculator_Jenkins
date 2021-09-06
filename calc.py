23429c85e42708e44b85ad02849b2ff4ffba35af
lekshmi <lekshmirajendran123@gmail.com>
2021-09-06 15:05:35 +0530

    Added Calc class

--- calc.py
+++ calc.py
@@ -2,4 +2,8 @@ class Calculator:
 	def __init__(self,num1,num2):
 		self.a = num1
 		self.b = num2
+	def return_a(self):
+		return self.a
+	def return_b(self):
+		return self.b
 	
--- main.py
+++ main.py
@@ -7,11 +7,15 @@ import sys
 
 print(sys.argv)
 if sys.argv[1] == "1":
-	add.add(1,2)
+	obj = Calculator(1,2)
+	add.add(obj.return_a(),obj.return_b())
 elif sys.argv[1] == "2":
-	sub.sub(4,2)
+	obj = Calculator(4,2)
+	sub.sub(obj.return_a(),obj.return_b())
 elif sys.argv[1] == "3":
-	mul.mul(4,2)
+	obj = Calculator(4,2)
+	mul.mul(obj.return_a(),obj.return_b())
 elif sys.argv[1] == "4":
-	div.div(4,2)
+	obj = Calculator(10,2)
+	div.div(obj.return_a(),obj.return_b())
 
